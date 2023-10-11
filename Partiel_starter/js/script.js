eacc4f013371511fbaf6197dfedb8b2eb715765e
B-Thibaut <55447253+B-Thibaut@users.noreply.github.com>
2023-10-11 15:48:35 +0200

    etape2:1 (à revoir)

--- Partiel_starter/js/script.js
+++ Partiel_starter/js/script.js
@@ -1,1 +1,36 @@
 console.log("Let's go !")
+let main = document.querySelector('main #characters');
+
+loadData();
+async function loadData() {
+    const response = await fetch('https://hp-api.onrender.com/api/characters');
+  const Personnages = await response.json();
+
+  const liste = Personnages.slice(0, 8);
+  console.log(liste)
+
+  let compte = 0;
+  for (const perso of liste) {
+    console.log(compte)
+    compte++;
+    
+    let article = document.createElement('article');
+
+    article.innerHTML = `
+        <td>
+            <figure>
+                <img src="${perso.image}" alt="${perso.name}" class="round-image">
+                <figcaption>${perso.name}</figcaption>
+            </figure>
+        </td>`;
+    if(compte == 3){
+        console.log("retour ligne")
+        article.innerHTML += `</tr>
+        <tr>`
+        compte=0;
+    }
+        
+    main.appendChild(article);
+
+    }
+}
